4bc91616171a6eda1e6550bfc8e71dafb322a916
Trushitha Narla <trushitha@squareup.com>
2021-09-07 17:44:35 -0700

    initial pass at loom button

---
+++ example/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+
+export default defineConfig({
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+  plugins: [reactRefresh()],
+  define: {
+    global: 'globalThis'
+  }
+});
